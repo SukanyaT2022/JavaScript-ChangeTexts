df9a09b03ea6dec073e56b6de7cb90eb1afd5e8b
Tiparpron Sukanya <tiparpron@Tiparprons-MacBook-Air.local>
2022-07-13 20:49:11 +0700

    first

---
+++ javaScript.js
@@ -0,0 +1,5 @@
+function change() {
+    document.getElementById('main').innerHTML="This is a vacation"
+    // chnage text from it'sholiday to  it's holiday time
+
+}
---
+++ new.js
@@ -0,0 +1,11 @@
+
+function replace(){
+document.getElementById('changeText').innerHTML= 'Let go hiking';
+}
+
+// function name(){
+//}
+
+// 2. how we call fuction
+// name()
+
